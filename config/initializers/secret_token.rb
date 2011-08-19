acef90e49be9921b973e2a441426155101033af3
thorncp <thorncp@gmail.com>
2011-08-18 23:01:52 -0800

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basestar::Application.config.secret_token = 'e0883c3bb4a02b8b4c39c534533b0c17f7242d5e5fabc05266ccbfa7a03552ed304032171edfd22f98c55759611536b1ef95f57a92ee60294d5d2edebc6fe983'
