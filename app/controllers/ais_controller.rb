009bfd0c3a4ce28655bdfd60e89aa21c3afc67c6
thorncp <thorncp@gmail.com>
2011-08-19 21:19:58 -0800

    Upload file.

--- app/controllers/ais_controller.rb
+++ app/controllers/ais_controller.rb
@@ -16,6 +16,8 @@ class AisController < ApplicationController
   def create
     @ai = current_user.ais.build(params[:ai])
 
+    @ai.source = File.read(params[:ai][:source].tempfile)
+
     if @ai.save
       redirect_to @ai, :notice => "Successfully created ai."
     else
