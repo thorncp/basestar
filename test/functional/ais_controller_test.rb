2cd362ccfe2bab399bfa34e821479a243c1c9875
thorncp <thorncp@gmail.com>
2011-09-02 17:25:33 -0800

    WhoaMG tests!!!!!

--- test/functional/ais_controller_test.rb
+++ test/functional/ais_controller_test.rb
@@ -1,54 +1,134 @@
 require 'test_helper'
 
 class AisControllerTest < ActionController::TestCase
-  def test_index
+  include Devise::TestHelpers
+
+  test "index renders index when user is signed in" do
+    sign_in users(:bob)
     get :index
-    assert_template 'index'
+    assert_template :index
+    assert_response :success
   end
 
-  def test_show
-    get :show, :id => Ai.first
-    assert_template 'show'
+  test "index redirects to public when user is not signed in" do
+    get :index
+    assert_redirected_to :action => "public"
   end
 
-  def test_new
+  test "show allows ai owner to view it" do
+    sign_in users(:bob)
+    get :show, :id => ais(:tigh)
+    assert_response :success
+  end
+
+  test "show allows non owner to view public ai" do
+    sign_in users(:bob)
+    get :show, :id => ais(:chief)
+    assert_response :success
+  end
+
+  test "show allows anonymous user to view public ai" do
+    get :show, :id => ais(:chief)
+    assert_response :success
+  end
+
+  test "show redirects to index when non owner tries to view private ai" do
+    sign_in users(:rex)
+    get :show, :id => ais(:tigh)
+    assert_redirected_to ais_path
+  end
+
+  test "show redirects to public when anonymous user tries to view private ai" do
+    get :show, :id => ais(:tigh)
+    assert_redirected_to public_ais_path
+  end
+
+  test "new allows acces to signed in user" do
+    sign_in users(:bob)
     get :new
-    assert_template 'new'
+    assert_template :new
   end
 
-  def test_create_invalid
-    Ai.any_instance.stubs(:valid?).returns(false)
-    post :create
-    assert_template 'new'
+  test "new redirects anonymous user to sign in" do
+    get :new
+    assert_redirected_to new_user_session_path
   end
 
-  def test_create_valid
+  test "create valid ai redirects to new record" do
     Ai.any_instance.stubs(:valid?).returns(true)
-    post :create
+    sign_in users(:bob)
+    
+    # todo: this fails if ai is not given, should probably fix that
+    post :create , :ai => {}
     assert_redirected_to ai_url(assigns(:ai))
   end
 
-  def test_edit
-    get :edit, :id => Ai.first
-    assert_template 'edit'
-  end
-
-  def test_update_invalid
+  test "create invalid ai rerenders the form" do
     Ai.any_instance.stubs(:valid?).returns(false)
-    put :update, :id => Ai.first
-    assert_template 'edit'
+    sign_in users(:bob)
+    post :create , :ai => {}
+    assert_template :new
   end
 
-  def test_update_valid
+  test "edit allows access to owner" do
+    sign_in users(:bob)
+    get :edit, :id => ais(:tigh)
+    assert_template :edit
+  end
+
+  test "edit redirects non owner to index" do
+    sign_in users(:bob)
+    get :edit, :id => ais(:chief)
+    assert_redirected_to ais_path
+  end
+
+  test "edit redirects anonymous user to sign in" do
+    get :edit, :id => ais(:chief)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "update valid ai redirects to record" do
     Ai.any_instance.stubs(:valid?).returns(true)
-    put :update, :id => Ai.first
+    sign_in users(:bob)
+    put :update , :id => ais(:tigh), :ai => {}
     assert_redirected_to ai_url(assigns(:ai))
   end
 
-  def test_destroy
-    ai = Ai.first
+  test "update invalid ai rerenders the form" do
+    Ai.any_instance.stubs(:valid?).returns(false)
+    sign_in users(:bob)
+    put :update , :id => ais(:tigh), :ai => {}
+    assert_template :edit
+  end
+
+  test "destroy delete the record" do
+    ai = ais(:tigh)
+    sign_in users(:bob)
     delete :destroy, :id => ai
-    assert_redirected_to ais_url
+    assert_redirected_to ais_path
     assert !Ai.exists?(ai.id)
+  end
+
+  test "download allows non owner to download public ai" do
+    # todo: figure out how to properly test send_data
+    sign_in users(:bob)
+    get :download, :id => ais(:chief)
+    assert_response :success
+  end
+
+  test "download allos anonymous user to download public ai" do
+    get :download, :id => ais(:chief)
+    assert_response :success
+  end
+
+  test "download redirects non owner of private ai to index" do
+    sign_in users(:rex)
+    get :download, :id => ais(:tigh)
+    assert_redirected_to ais_path
+  end
+
+  test "download redirects anonymous user to public ais path for private ai" do
+    get :download, :id => ais(:tigh)
+    assert_redirected_to public_ais_path
   end
 end
