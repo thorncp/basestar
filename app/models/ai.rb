2752b8e21987ea23fdd3e2bebafbe9f4a3004a02
thorncp <thorncp@gmail.com>
2011-09-03 20:31:48 -0800

    Ai tests.

--- app/models/ai.rb
+++ app/models/ai.rb
@@ -3,15 +3,15 @@ class Ai < ActiveRecord::Base
 
   belongs_to :user
 
-  validates_presence_of :name, :source, :user
+  validates_presence_of :name, :source, :user, :file_name
   validates_uniqueness_of :name
-  validates_format_of :file_name, :with => /\.rb$/, :on => :create, :message => "must be a ruby file"
+  validates_format_of :file_name, :with => /\.rb$/, :message => "must be a ruby file"
 
   before_validation :set_name_from_file_name
 
   scope :public, where(:public => true)
 
   def set_name_from_file_name
-    self.name = File.basename(self.file_name, ".*").camelize
+    self.name = File.basename(self.file_name, ".*").camelize if self.file_name
   end
 end
--- test/unit/ai_test.rb
+++ test/unit/ai_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class AiTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Ai.new.valid?
+  test "name is required" do
+    ai = ais(:tigh)
+    ai.name = nil
+    assert !ai.valid?
+    assert ai.errors.include?(:name)
+  end
+
+  test "name must be unique" do
+    ai = ais(:chief)
+    ai.name = ais(:tigh).name
+    assert !ai.valid?
+    assert ai.errors.include?(:name)
+  end
+
+  test "file_name is required" do
+    ai = ais(:tigh)
+    ai.file_name = nil
+    assert !ai.valid?
+    assert ai.errors.include?(:file_name)
+  end
+
+  test "file_name must end in .rb" do
+    ai = ais(:tigh)
+    ai.file_name = "derp.h"
+    assert !ai.valid?
+    assert ai.errors.include?(:file_name)
+  end
+
+  test "source is required" do
+    ai = ais(:tigh)
+    ai.source = nil
+    assert !ai.valid?
+    assert ai.errors.include?(:source)
+  end
+
+  test "user is required" do
+    ai = ais(:tigh)
+    ai.user = nil
+    assert !ai.valid?
+    assert ai.errors.include?(:user)
   end
 end
